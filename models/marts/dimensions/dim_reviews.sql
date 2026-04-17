3e2517bf7a60f5d34afb65733c3ee293e5f63c9f
Laurence Mattew <lmatlifts@gmail.com>
2026-04-17 08:22:33 +0000

    added review dimension and some changes

---
+++ models/marts/dimensions/dim_reviews.sql
@@ -0,0 +1,27 @@
+with orders as (
+    select * from {{ ref('stg_olist__orders') }}
+),
+
+reviews as (
+    select * from {{ ref('stg_olist__order_reviews') }}
+),
+
+final as (
+
+        select 
+
+            orders.order_id,
+            reviews.review_id,
+            reviews.score as score,
+            coalesce(reviews.title, ' / ') as comment_title,
+            coalesce(reviews.message, ' /') as comment_message,
+            reviews.creation_date as created_at,
+            reviews.answer_at 
+
+
+        from orders
+        left join reviews on orders.order_id = reviews.order_id
+
+)
+
+select * from final
