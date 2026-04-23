0c755dbdf112208bf349620053abe087c1c34383
Loïc Mattioli <lmatlifts@gmail.com>
2026-04-23 11:42:27 +0000

    refactor: adjust grain for fct_order_items (1 row = 1 item) and fct_orders (1 row = 1 order)

--- models/marts/facts/fct_order_items.sql
+++ models/marts/facts/fct_order_items.sql
@@ -1,28 +1,27 @@
 {{ config(materialized="incremental", incremental_strategy="append") }}
 
 with 
-    products as ( select * from {{ ref('dim_products') }}),
     orders as (select * from {{ ref('stg_olist__orders') }}),
     order_items as (select * from {{ ref('stg_olist__order_items') }}),
-
 
     final as (
 
         select 
 
+            order_items.order_items_pk,
             orders.order_id,
             orders.customer_id,
             order_items.seller_id,
+            order_items.product_id,
             order_items.price,
             order_items.freight_amount as freight,
             order_items.price + order_items.freight_amount as total_amount,
             orders.purchased_at as purchased_at,
             datediff(day, order_items.shipping_limit_date, orders.delivered_to_customer_at) > 0 as is_late,
             datediff(day, orders.purchased_at, orders.delivered_to_customer_at) as days_to_delivery
-
+            
         from orders 
-        join order_items on order_items.order_id = orders.order_id 
-        
+        join order_items on order_items.order_id = orders.order_id         
     )
 
     select * from final
---
+++ models/marts/facts/fct_orders.sql
@@ -0,0 +1,27 @@
+{{ config(materialized="incremental", incremental_strategy="append") }}
+
+with 
+    order_item as (select * from {{ ref('stg_olist__order_items') }}),
+    orders as (select * from {{ ref('stg_olist__orders') }}),
+
+final as (
+
+        select
+
+            order_item.order_id,
+            orders.customer_id,
+            orders.purchased_at,
+            count(order_item.order_items_pk) as nb_items,
+            sum(order_item.price + order_item.freight_amount) as total_order_amount
+        
+        from orders
+        join order_item on order_item.order_id = orders.order_id
+
+        group by order_item.order_id, orders.customer_id, orders.purchased_at
+)
+
+select * from final
+
+{% if is_incremental() %}
+     where purchased_at > (select max(purchased_at) from {{ this }})
+{% endif %}
